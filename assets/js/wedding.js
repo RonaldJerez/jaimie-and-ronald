65fb6d196fe1fe821a6d3b1c234eb2fc9242fcf2
dlanor15 <dlanor@gmail.com>
2013-06-05 02:30:22 -0400

    Google maps not being centered because it was being loaded while the container was hidden. Showing it on demand fixes the issue.
    Also put some notices for Internet Explorer 7+8 users (dont think it
    was needed but just in case lets cover all the bases).

--- assets/js/wedding.js
+++ assets/js/wedding.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	var hash = window.location.hash;
+	var map_loaded = false;
 	
 	if (hash) {
 		$("#nav .nav a[href='"+hash+"']").trigger("click");
@@ -12,7 +13,34 @@ $(document).ready(function(){
 	$("#nav .nav").on("shown", function(e){
 		var pos = $($(e.target).attr("href")).position().top - 50;
 		$("html, body").animate({scrollTop: pos}, 300);
+		
+		if (!map_loaded && $(e.target).attr("href") == "#info") {
+			$("#map").html('<iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps/ms?t=m&amp;msa=0&amp;msid=213480164619931622304.0004dcb02d7385eac6315&amp;source=embed&amp;ie=UTF8&amp;ll=40.51928,-74.347143&amp;spn=0.027861,0.045748&amp;z=14&amp;output=embed"></iframe>');
+			map_loaded = true;
+		}
 	});
+	
+	// internet explorer always causing trouble
+	if ($.browser.msie) {
+		if ($.browser.version == 8) {
+			$(".accordion-toggle").on("click", function(e){
+				var target = $($(this).attr("href"));
+				
+				if (target.is("in"))
+					target.removeClass("in").hide();
+				else
+					target.addClass("in").show();
+			});
+			
+			$("body").css("margin-top", 48);
+			$("#badbrowser").show().find(".ie8").show();
+		} else if ($.browser.version < 8) {
+			$("#badbrowser .ie7").show();
+			$("#badbrowser").show().siblings().hide();
+		}
+	}
+	
+	
 });
 
 var setAffix = function() {
